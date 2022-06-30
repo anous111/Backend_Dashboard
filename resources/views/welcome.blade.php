400254f4d5d01928a7f41021e7d4721c49b2dea4
Anis Safia <anissafia90@gmail.com>
2022-06-30 17:41:36 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.master')
+
+@section('title')
+  Dashboard
+@endsection
+
+@section('css')
+  
+@endsection
+
+@section('title1')
+  Home
+@endsection
+
+@section('title2')
+  Dashboard
+@endsection
+
+@section('content')
+  
+@endsection
+
+@section('script')
+  
+@endsection
